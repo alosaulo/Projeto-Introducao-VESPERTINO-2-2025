1e90d47aadf0326751b78e58f62ab005d65e5ee9
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2025-08-04 13:09:37 -0500

    Projeto Inicial

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Determinar controle do player
+    //Pegar o rigidbody do player
+    //Usar o rigidbody para controlar o player
+
+    /*
+     * W - para cima
+     * S - para baixo
+     * A - para esquerda
+     * D - para direita
+     */
+
+    public float speed = 5.2f; // Velocidade do player
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
