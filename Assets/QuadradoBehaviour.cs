2aff38096b6e1a257048fa6da4e5a0e1cd2fe2e1
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2025-08-15 15:05:27 -0300

    Colisão triangulo

--- Assets/QuadradoBehaviour.cs
+++ Assets/QuadradoBehaviour.cs
@@ -4,6 +4,9 @@ public class QuadradoBehaviour : MonoBehaviour
 {
     public GameObject tiroPrefab; // Prefab do tiro
 
+    public float tiroInterval = 0.5f; // Intervalo entre tiros em segundos
+    float timerTiro; // Temporizador para controlar o intervalo de tiro
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +16,23 @@ public class QuadradoBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Instantiate(tiroPrefab, transform.position, Quaternion.identity);
+        if(timerTiro >= tiroInterval) 
+        { 
+            GameObject tiro = Instantiate
+                                (tiroPrefab, transform.position, Quaternion.identity);
+            
+            Rigidbody2D tiroRb = tiro.GetComponent<Rigidbody2D>();
+            
+            Transform playerPosition = 
+                GameObject.FindGameObjectWithTag("Player").transform;
+            
+            Vector3 direcaoTiro = playerPosition.position - transform.position;
+
+            tiroRb.linearVelocity = direcaoTiro * 5f;
+            
+            timerTiro = 0; // Reseta o temporizador após disparar
+        }
+        // Incrementa o temporizador com o tempo desde o último frame
+        timerTiro += Time.deltaTime; 
     }
 }
